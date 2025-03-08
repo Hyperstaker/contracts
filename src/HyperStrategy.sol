18079e00a5892f420211da6e64aa12e917e113c9
Prajjawalk <prajjawalkhandelwal@gmail.com>
2025-03-08 23:41:07 +0530

    Add: strategy

---
+++ src/HyperStrategy.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {AccessControlUpgradeable} from "lib/allo-v2.1/lib/openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import {UUPSUpgradeable} from "lib/allo-v2.1/lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {BaseStrategy} from "strategies/BaseStrategy.sol";
+import {IAllo} from "lib/allo-v2.1/contracts/core/interfaces/IAllo.sol";
+
+
+contract HyperStrategy is AccessControlUpgradeable, UUPSUpgradeable, BaseStrategy {
+
+    // Roles
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+
+    error NOOP();
+
+    function initialize(uint256 _poolId, bytes memory _data) public initializer {
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+
+        address _manager = abi.decode(_data, (address));
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MANAGER_ROLE, _manager);
+        __BaseStrategy_init(_poolId);
+        emit Initialized(_poolId, _data);
+    }
+    
+    /// ===============================
+    /// ========= Constructor =========
+    /// ===============================
+    constructor(address _allo, string memory _name) BaseStrategy(_allo, _name) {}
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(MANAGER_ROLE) {}
+
+    /// @notice Allocate funds hyperfund and hyperstaker pools
+    /// @param _data The data to decode
+    /// @param _sender The sender
+    function _allocate(bytes memory _data, address _sender)
+        internal
+        virtual
+        override
+        onlyRole(MANAGER_ROLE)
+    {
+      (address[] memory _recipients, uint256[] memory _amounts) = abi.decode(_data, (address[], uint256[]));
+
+      // Assert recipient and amounts length are equal
+      if (_recipients.length != _amounts.length) {
+          revert ARRAY_MISMATCH();
+      }
+
+      IAllo.Pool memory pool = allo.getPool(poolId);
+      for (uint256 i; i < _recipients.length; ++i) {
+          uint256 _amount = _amounts[i];
+          address _recipientAddress = _recipients[i];
+
+          _transferAmount(pool.token, _recipientAddress, _amount);
+
+          emit Allocated(_recipientAddress, _amount, pool.token, _sender);
+      }
+
+    }
+
+    function _distribute(address[] memory _recipientIds, bytes memory _recipientAmounts, address _sender)
+        internal
+        virtual
+        override
+    {
+      revert NOOP();
+    }
+
+    function _getRecipientStatus(address) internal view virtual override returns (Status) {
+        revert NOOP();
+    }
+
+    function _isValidAllocator(address _allocator) internal view virtual override returns (bool) {}
+
+    function _registerRecipient(bytes memory _data, address _sender) internal virtual override returns (address) {}
+
+    function _getPayout(address _recipientId, bytes memory _data)
+        internal
+        view
+        virtual
+        override
+        returns (PayoutSummary memory)
+    {}
+
+    receive() external payable {}
+}
