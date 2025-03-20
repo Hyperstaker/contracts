ea7b4210c0c999656da2f7ecd39752e4da986fdd
Prajjawalk <prajjawalkhandelwal@gmail.com>
2025-03-20 12:07:53 +0530

    Add: _afterIncreasePoolAmount

--- src/HyperStrategy.sol
+++ src/HyperStrategy.sol
@@ -4,15 +4,24 @@ pragma solidity 0.8.19;
 import {AccessControl} from "lib/allo-v2.1/lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 import {BaseStrategy} from "strategies/BaseStrategy.sol";
 import {IAllo} from "lib/allo-v2.1/contracts/core/interfaces/IAllo.sol";
+import {IHypercertToken} from "./interfaces/IHypercertToken.sol";
+import {IHyperfund} from "./interfaces/IHyperfund.sol";
 
 contract HyperStrategy is AccessControl, BaseStrategy {
     // Roles
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
+    IHyperfund public hyperfund;
+
+    IHypercertToken public hypercertMinter;
+
     error NOOP();
 
     function initialize(uint256 _poolId, bytes memory _data) external virtual override {
-        address _manager = abi.decode(_data, (address));
+        (address _manager, address _hyperfund) = abi.decode(_data, (address, address));
+
+        hyperfund = IHyperfund(_hyperfund);
+        hypercertMinter = IHypercertToken(hyperfund.hypercertMinter());
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MANAGER_ROLE, _manager);
@@ -47,6 +56,22 @@ contract HyperStrategy is AccessControl, BaseStrategy {
         }
     }
 
+    function _afterIncreasePoolAmount(uint256 _amount) internal virtual override {
+        IAllo.Pool memory pool = allo.getPool(poolId);
+        uint256 hypercertFraction = hyperfund.hypercertId();
+        int256 multiplier = hyperfund.tokenMultipliers(pool.token);
+        uint256 units;
+        if (multiplier > 0) {
+            units = _amount * uint256(multiplier);
+        } else {
+            units = _amount / uint256(-multiplier);
+        }
+
+        uint256 availableSupply = hypercertMinter.unitsOf(hypercertFraction);
+        require(availableSupply >= units);
+        _mintFraction(tx.origin, units, hypercertFraction);
+    }
+
     function _distribute(address[] memory _recipientIds, bytes memory _recipientAmounts, address _sender)
         internal
         virtual
@@ -70,6 +95,13 @@ contract HyperStrategy is AccessControl, BaseStrategy {
         override
         returns (PayoutSummary memory)
     {}
+
+    function _mintFraction(address account, uint256 units, uint256 hypercertId) internal {
+        uint256[] memory newallocations = new uint256[](2);
+        newallocations[0] = hypercertMinter.unitsOf(hypercertId) - units;
+        newallocations[1] = units;
+        hypercertMinter.splitFraction(account, hypercertId, newallocations);
+    }
 
     receive() external payable {}
 }
